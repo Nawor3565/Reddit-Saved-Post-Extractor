40184018ed0693286e8218b983e974ddf6dffa12
Nawor3565 <nawor3565@gmail.com>
2020-04-22 16:22:58 -0400

    Add files via upload

---
+++ post-extractor.py
@@ -0,0 +1,48 @@
+import csv
+from datetime import datetime
+import time
+from psaw import PushshiftAPI
+
+api = PushshiftAPI()
+
+with open('saved_posts.csv', 'r', newline='') as f:  # Opens the CSV files from your Reddit data request
+    reader = csv.reader(f)
+    data = list(reader)
+    with open('fixed_posts.csv', 'a', newline='') as f2:  # Opens/creates the new CSV where the post info will go
+        writer = csv.writer(f2)
+        writer.writerow(["Date/Time", "Subreddit", "Post Title", "Link", "Is Self-Post?", "URL", "Post ID"])  # Writes name for each column
+        for i in data:
+            for attempt in range(3):  # If error, will retry up to three times
+                try:
+                    i = str(i)
+                    i = i[2:-2]  # Gets post ID
+                    search = api.search_submissions(ids=i, limit=1, aggs='title')  # Searches Pushshift for matching post
+                    for post in search:
+                        subreddit = post.subreddit
+                        title = post.title
+                        title = title.encode("ascii", errors="ignore").decode()  # Removes special chars like emoji
+                        link = post.full_link
+                        date = post.created_utc
+                        date = datetime.fromtimestamp(date).strftime('%Y-%m-%d %H:%M:%S')  # Makes date readable
+                        is_self = post.is_self
+                        media = ""
+                        print(subreddit)
+                        print(title)
+                        print(link)
+                        print(date)
+                        if is_self is False:  # If post is text-only, don't try to get whatever it links to
+                            media = post.url
+                            print(media)
+                        writer.writerow([date, subreddit, title, link, str(is_self), media, i])  # Writes data to new line
+                        f2.flush()  # Forces file to save
+                        print("Saved, moving to next item")
+                    time.sleep(1)
+                except:
+                    print("Error: Trying again in 10 seconds (max 3 retries)")
+                    time.sleep(10)
+                    continue
+                else:
+                    break
+
+f2.close()
+f.close()
