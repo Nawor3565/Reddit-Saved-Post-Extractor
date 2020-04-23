e343b10897bbaeceafe529f3b683c6d126062a7b
Nawor3565 <nawor3565@gmail.com>
2020-04-23 15:27:07 -0400

    Script will no longer close if a comment is deleted.

--- comment-extractor.py
+++ comment-extractor.py
@@ -2,6 +2,7 @@ import csv
 from datetime import datetime
 import time
 import praw
+from praw.exceptions import ClientException
 
 reddit = praw.Reddit(user_agent='Saved comment scraper',
                      client_id='CLIENT_ID', client_secret="CLIENT_SECRET",
@@ -10,35 +11,43 @@ reddit = praw.Reddit(user_agent='Saved comment scraper',
 with open('saved_comments.csv', 'r', newline='') as f:  # Opens the CSV files from your Reddit data request
     next(f)
     progress = 1
+    errors = 0
     reader = csv.reader(f)
     first_column = next(zip(*reader))
     data = list(first_column)
     print(data)
-    with open('fixed_comments.csv', 'a', newline='') as f2:  # Opens/creates the new CSV where the post info will go
+    with open('fixed_comments.csv', 'a', newline='', encoding='utf-8') as f2:  # Opens/creates the new CSV where the post info will go
         writer = csv.writer(f2)
         writer.writerow(["Date/Time", "Subreddit", "Link", "Comment ID", "Body"])  # Writes name for each column
         for i in data:
             i = str(i)
             print(i)
-            comment = reddit.comment(id=i)
+            try:
+                comment = reddit.comment(id=i)
 
-            subreddit = comment.subreddit
-            body = comment.body
-            body = body.encode("ascii", errors="ignore").decode()  # Removes special chars like emoji
-            link = comment.permalink
-            link = "https://reddit.com" + link
-            date = comment.created_utc
-            date = datetime.fromtimestamp(date).strftime('%Y-%m-%d %H:%M:%S')  # Makes date readable
-            print(subreddit)
-            print(link)
-            print(date)
-            print(body)
+                subreddit = comment.subreddit
+                body = comment.body
+                body = body.encode("ascii", errors="ignore").decode()  # Removes special chars like emoji
+                link = comment.permalink
+                link = "https://reddit.com" + link
+                date = comment.created_utc
+                date = datetime.fromtimestamp(date).strftime('%Y-%m-%d %H:%M:%S')  # Makes date readable
+                print(subreddit)
+                print(link)
+                print(date)
+                print(body)
+                writer.writerow([date, subreddit, link, i, body])  # Writes data to new line
+                print("Saved comment #" + str(progress) + ", continuing to next item")
 
-            writer.writerow([date, subreddit, link, i, body])  # Writes data to new line
+            except ClientException:
+                print("No comment data returned for #" + str(progress) + ". Skipping...")
+                writer.writerow(["NO DATA", "NO DATA", "NO DATA", i, "NO DATA"])
+                errors += 1
+
             f2.flush()  # Forces file to save
-            print("Saved comment #" + str(progress) + ", continuing to next item")
             progress += 1
             time.sleep(2)
 
 f2.close()
 f.close()
+print("FINISHED. Saved" + str(progress) + "comments, with" + str(errors) + "errors.")
